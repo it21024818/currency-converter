545cac9d9f3739a73524367ce784103bb266b24a
it21024818 <it21024818@my.sliit.lk>
2024-07-03 01:30:32 +0530

    transfer history component updated

--- app/components/TransferHistory.js
+++ app/components/TransferHistory.js
@@ -2,22 +2,32 @@
 
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import CurrencyConverter from "./CurrencyConverter";
+import { Box, Typography, List, ListItem, ListItemText, ListItemSecondaryAction, IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import CurrencyConverter from './CurrencyConverter';
 
 const TransferHistory = () => {
   const [transfers, setTransfers] = useState([]);
 
   useEffect(() => {
     const fetchTransfers = async () => {
-      const response = await axios.get(`${process.env.SERVER_URL}/transfers`);
-      setTransfers(response.data);
+      try {
+        const response = await axios.get(`${process.env.SERVER_URL}/transfers`);
+        setTransfers(response.data);
+      } catch (error) {
+        console.error('Error fetching transfers:', error);
+      }
     };
     fetchTransfers();
   }, []);
 
   const handleDelete = async (id) => {
-    await axios.delete(`${process.env.SERVER_URL}/transfers/${id}`);
-    setTransfers(transfers.filter(transfer => transfer._id !== id));
+    try {
+      await axios.delete(`${process.env.SERVER_URL}/transfers/${id}`);
+      setTransfers(transfers.filter(transfer => transfer._id !== id));
+    } catch (error) {
+      console.error('Error deleting transfer:', error);
+    }
   };
 
   const addNewTransfer = (newTransfer) => {
@@ -25,18 +35,24 @@ const TransferHistory = () => {
   };
 
   return (
-    <div>
-    <CurrencyConverter onNewTransfer={addNewTransfer} />
-      <h2>Transfer History</h2>
-      <ul>
+    <Box sx={{ maxWidth: 600, margin: 'auto', padding: 2 }}>
+      <CurrencyConverter onNewTransfer={addNewTransfer} /><br></br>
+      <Typography variant="h4" gutterBottom>Transfer History</Typography>
+      <List>
         {transfers.map(transfer => (
-          <li key={transfer._id}>
-            {transfer.fromCountry} to {transfer.toCountry}: {transfer.amount} ={'>'} {transfer.convertedAmount}
-            <button onClick={() => handleDelete(transfer._id)}>Revoke</button>
-          </li>
+          <ListItem key={transfer._id} disablePadding>
+            <ListItemText
+              primary={`${transfer.fromCountry} to ${transfer.toCountry}: ${transfer.amount} ${transfer.fromCountry} => ${transfer.convertedAmount} ${transfer.toCountry}`}
+            />
+            <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="delete" onClick={() => handleDelete(transfer._id)}>
+                <DeleteIcon sx={{ color: 'red'}}/>
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
         ))}
-      </ul>
-    </div>
+      </List>
+    </Box>
   );
 };
 
